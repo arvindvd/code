40306cd0f0fc9cd4a5f2abbe4d7f5c2c6412159a
arvindvd <133097696+arvindvd@users.noreply.github.com>
2024-05-20 18:29:16 +0530

    Update post.js

--- post.js
+++ post.js
@@ -2,14 +2,31 @@
 var xhr = new XMLHttpRequest();
 
 
-xhr.open ('POST', 'https://thetestingworldapi.com/api/studentsDetails', true);
+xhr.open ('POST', 'https://jsonplaceholder.typicode.com/users', true);
 {
-  "id": 1,
-  "first_name": "Arving",
-  "middle_name": "Vijayanand",
-  "last_name": "Dubey",
-  "date_of_birth": "02/07/1980"
-}
+  
+    "id": 11,
+    "name": "arvind dubey",
+    "username": "arvindd",
+    "email": "arv@gmail.com",
+    "address": {
+      "street": "Kattie Turnpike",
+      "suite": "Suite 198",
+      "city": "Lebsackbury",
+      "zipcode": "31428-2261",
+      "geo": {
+        "lat": "18.2386",
+        "lng": "24.2232"
+      }
+    },
+    "phone": "024-648-3804",
+    "website": "ambrose.net",
+    "company": {
+      "name": "Hoeger LLC",
+      "catchPhrase": "Centralized empowering task-force",
+      "bs": "target end-to-end models"
+    }
+  }
 
 xhr. onreadystatechange = function() {
       if(xhr.onreadystatechange === 4 && xhr.status ===200) {
