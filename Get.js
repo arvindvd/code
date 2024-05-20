49da62fcd78357dcde401b997ffb9bcc23a194e3
arvindvd <133097696+arvindvd@users.noreply.github.com>
2024-05-20 18:25:51 +0530

    Update Get.js

--- Get.js
+++ Get.js
@@ -2,7 +2,7 @@
 var xhr = new XMLHttpRequest();
 
 
-xhr.open ('GET', 'https://thetestingworldapi.com/api/studentsDetails/4165325', true);
+xhr.open ('GET', 'https://https://jsonplaceholder.typicode.com/users/1', true);
 xhr. onreadystatechange = function() {
       if(xhr.onreadystatechange === 4 && xhr.status ===200) {
          var response = JSON.parse(xhr .responseText)
